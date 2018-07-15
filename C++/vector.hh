774f597606dcef1a98c5130e938512471e4b217c
xpslvs <22846562+xpslvs@users.noreply.github.com>
2018-07-15 19:41:16 +0200

    Added some functions and fixed naming

--- C++/vector.hh
+++ C++/vector.hh
@@ -1,4 +1,4 @@
-/* vector.hh */
+/* Vector.hh */
 #ifndef VECTOR_HH
 #define VECTOR_HH
 
@@ -6,249 +6,265 @@
 #include <cstring>
 #include <initializer_list>
 
-/* # Elements, 
- * Scalar type
- */
-template <unsigned N, typename T>
+template <int N, class T>
 class Vector
 {
 public:
-	Vector(void)
-	{
-		std::memset(this->_elem, 0, N * sizeof(T));
-	}
+    Vector(void);
+    Vector(const T *);
+    Vector(const std::initializer_list<T>&);
+    template <class U>
+    operator Vector<N, U>(void) const;
+    template <int M, class U>
+    explicit operator Vector<M, U>(void) const;
+    inline const T& operator[](int) const;
+    inline       T& operator[](int);
 
-	Vector(const T v[N])
-	{
-		std::memcpy(this->_elem, v, N * sizeof(T));
-	}
+    T      dot(const Vector&) const;
+    Vector cross(const Vector&) const;
+    T      quadrance(const Vector&) const;
+    auto   distance(const Vector&) const;
+    Vector proj(const Vector&) const;
+    Vector perp(const Vector&) const;
+    Vector normalize(void) const;
+    T      norm(void) const;
+    auto   magnitude(void) const;
+    auto   angle(const Vector&) const;
 
-	Vector(const std::initializer_list<T> &aggregate)
-	{
-		auto iter = aggregate.begin();
-		for(unsigned i = 0; i < N; ++i)
-		{
-			this->_elem[i] = iter != aggregate.end() ? *iter++ : (T)0;
-		}
-	}
-
-	Vector add(const Vector &v)
-	{
-		for(unsigned i = 0; i < N; ++i)
-		{
-			(*this)[i] += v.elem(i);
-		}
-		return *this;
-	}
-	
-	Vector subtract(const Vector &v)
-	{
-		return this->add(-v);
-	}
-	
-	/* Scalar multiplication
-	 */
-	Vector multiply(const T &c)
-	{
-		for(unsigned i = 0; i < N; ++i)
-		{
-			(*this)[i] *= c;
-		}
-		return *this;
-	}
-
-	Vector divide(const T &c)
-	{
-		for(unsigned i = 0; i < N; ++i)
-		{
-			(*this)[i] /= c;
-		}
-		return *this;
-	}
-
-	/* Scalar/dot product
-	 */
-	T dot(const Vector &v) const
-	{
-		T p = (T)0;
-		for(unsigned i = 0; i < N; ++i)
-		{
-			p += this->elem(i) * v.elem(i);
-		}
-		return p;
-	}
-	
-	Vector<3, T> cross(const Vector<3, T> &v) const
-	{
-		static_assert(N == 3, "Cross product is only defined for vectors of size 3");
-
-		Vector<3, T> u{};
-		u[0] = this->elem(1) * v.elem(2) - this->elem(2) * v.elem(1);
-		u[1] = this->elem(2) * v.elem(0) - this->elem(0) * v.elem(2);
-		u[2] = this->elem(0) * v.elem(1) - this->elem(1) * v.elem(0);
-		return u;
-	}
-
-	/* Project vector v onto this
-	 */
-	Vector project(const Vector &v) const
-	{
-		return (*this) * (this->dot(v) / this->norm());
-	}
-	
-	Vector normalize(void) const
-	{
-		return (*this) / this->magnitude();
-	}
-
-	T angle(const Vector &v) const
-	{
-		return (T)std::acos(this->dot(v) / (this->magnitude() * v.magnitude()));
-	}
-
-	bool equals(const Vector &v) const
-	{
-		return !std::memcmp(this->_elem, v._elem, N * sizeof(T));
-	}
-	
-	T magnitude(void) const
-	{
-		return (T)std::sqrt(this->norm());
-	}
-	
-	/* Magnitude squared
-	 */
-	T norm(void) const
-	{
-		return this->dot(*this);
-	}
-
-	/* Casting to a different size can be done explicitly 
-	 */
-	template <unsigned M>
-	explicit operator Vector<M, T>(void) const
-	{
-		Vector<M, T> v{};
-		for(unsigned i = 0; i < (M < N ? M : N); ++i)
-		{
-			v[i] = this->elem(i);
-		}
-		return v;
-	}
-
-	template <unsigned M, typename S>
-	explicit operator Vector<M, S>(void) const
-	{
-		return (Vector<M, S>)((Vector<M, T>)(*this));
-	}
-
-	/* Casting to a different scalar type can be done implicitly
-	 */
-	template <typename S>
-	operator Vector<N, S>(void) const
-	{
-		Vector<N, S> v;
-		for(unsigned i = 0; i < N; ++i)
-		{
-			v[i] = (S)this->elem(i);
-		}
-		return v;
-	}
-
-	/* No index checking is done when using element accessors
-	 */
-	inline T elem(unsigned i) const
-	{
-		return this->_elem[i];
-	}
-
-	inline T& operator [](unsigned i)
-	{
-		return this->_elem[i];
-	}
-
-	inline T x(void) const
-	{
-		return this->elem(0); 
-	}
-
-	inline T y(void) const
-	{
-		return this->elem(1); 
-	}
-
-	inline T z(void) const
-	{
-		return this->elem(2);
-	}
-
-	inline T w(void) const
-	{
-		return this->elem(3);
-	}
-
-	inline Vector operator +(void) const
-	{
-		return (*this);
-	}
-	
-	inline Vector operator -(void) const
-	{
-		return (*this) * (-1);
-	}
-	
-	inline Vector operator +(const Vector &v) const 
-	{
-		return ((Vector)(*this)).add(v);
-	}
-	
-	inline Vector operator -(const Vector &v) const
-	{
-		return ((Vector)(*this)).subtract(v);
-	}
-	
-	inline Vector operator *(const T &c) const
-	{
-		return ((Vector)(*this)).multiply(c);
-	}
-
-	inline Vector operator /(const T &c) const
-	{
-		return ((Vector)(*this)).divide(c);
-	}
-	
-	inline bool operator ==(const Vector &v) const
-	{
-		return this->equals(v);
-	}
-
-	inline bool operator !=(const Vector &v) const
-	{
-		return !this->equals(v);
-	}
-	
-	inline Vector operator +=(const Vector &v)
-	{
-		return this->add(v);
-	}
-	
-	inline Vector operator -=(const Vector &v)
-	{
-		return this->subtract(v);
-	}
-	
-	inline Vector operator *=(const T &c)
-	{
-		return this->multiply(c);
-	}
-
-	inline Vector operator /=(const T &c)
-	{
-		return this->divide(c);
-	}
+    inline Vector operator +(void) const;
+    inline Vector operator -(void) const;
+    inline Vector operator +(const Vector&) const;
+    inline Vector operator -(const Vector&) const;
+    inline Vector operator *(const T&) const;
+    inline Vector operator /(const T&) const;
+    inline bool   operator ==(const Vector&) const;
+    inline bool   operator !=(const Vector&) const;
+    Vector operator +=(const Vector&);
+    Vector operator -=(const Vector&);
+    Vector operator *=(const T&);
+    Vector operator /=(const T&);
 private:
-	T _elem[N];
+    T _element[N];
 };
+
+template <int N, class T>
+Vector<N, T>::Vector(void)
+{
+    std::memset(this->_element, T(), N * sizeof(T));
+}
+
+template <int N, class T>
+Vector<N, T>::Vector(const T *element)
+{
+    std::memcpy(this->_element, element, N * sizeof(T));
+}
+
+template <int N, class T>
+Vector<N, T>::Vector(const std::initializer_list<T> &aggregate)
+{
+    auto iter = aggregate.begin();
+    for(int i = 0; i < N; ++i)
+    {
+        this->_element[i] = (iter != aggregate.end()) ? *(iter++) : T();
+    }
+}
+
+template <int N, class T>
+template <class U>
+Vector<N, T>::operator Vector<N, U>(void) const
+{
+    Vector<N, U> v{};
+    for(int i = 0; i < N; ++i)
+    {
+        v[i] = static_cast<U>((*this)[i]);
+    }
+    return v;
+}
+
+template <int N, class T>
+template <int M, class U>
+Vector<N, T>::operator Vector<M, U>(void) const
+{
+    Vector<M, U> v{};
+    for(int i = 0; i < N && i < M; ++i)
+    {
+        v[i] = static_cast<U>((*this)[i]);
+    }
+    return v;
+}
+
+template <int N, class T>
+inline const T& Vector<N, T>::operator[](int index) const
+{
+    return this->_element[index];
+}
+
+template <int N, class T>
+inline T& Vector<N, T>::operator[](int index)
+{
+    return this->_element[index];
+}
+
+template <int N, class T>
+T Vector<N, T>::dot(const Vector<N, T>& v) const
+{
+    T p = T();
+    for(int i = 0; i < N; ++i)
+    {
+        p += (*this)[i] * v[i];
+    }
+    return p;
+}
+
+ /* Defined only when N = 3
+  */
+template <int N, class T>
+Vector<N, T> Vector<N, T>::cross(const Vector<N, T>& v) const
+{
+    Vector<3, T> u{};
+    u[0] = (*this)[1] * v[2] - (*this)[2] * v[1];
+    u[1] = (*this)[2] * v[0] - (*this)[0] * v[2];
+    u[2] = (*this)[0] * v[1] - (*this)[1] * v[0];
+    return u;
+}
+
+template <int N, class T>
+T Vector<N, T>::quadrance(const Vector<N, T>& v) const
+{
+    return ((*this) - v).norm();
+}
+
+template <int N, class T>
+auto Vector<N, T>::distance(const Vector<N, T>& v) const
+{
+    return ((*this) - v).magnitude();
+}
+
+/* Project v onto this
+ */
+template <int N, class T>
+Vector<N, T> Vector<N, T>::proj(const Vector<N, T>& v) const
+{
+    return ((*this) * (this->dot(v))) / this->norm();
+}
+
+template <int N, class T>
+Vector<N, T> Vector<N, T>::perp(const Vector<N, T>& v) const
+{
+    return v - this->proj(v);
+}
+
+template <int N, class T>
+Vector<N, T> Vector<N, T>::normalize(void) const
+{
+    return (*this) / this->magnitude();
+}
+
+template <int N, class T>
+auto Vector<N, T>::angle(const Vector<N, T>& v) const
+{
+    return std::acos(this->dot(v) / (this->magnitude() * v.magnitude()));
+}
+
+template <int N, class T>
+T Vector<N, T>::norm(void) const
+{
+    return this->dot(*this);
+}
+
+template <int N, class T>
+auto Vector<N, T>::magnitude(void) const
+{
+    return std::sqrt(this->norm());
+}
+
+template <int N, class T>
+inline Vector<N, T> Vector<N, T>::operator +(void) const
+{
+    return (*this);
+}
+
+template <int N, class T>
+inline Vector<N, T> Vector<N, T>::operator -(void) const
+{
+    return (*this) * (-1);
+}
+
+template <int N, class T>
+Vector<N, T> Vector<N, T>::operator +(const Vector<N, T>& v) const
+{
+    return Vector<N, T>(*this) += v;
+}
+
+template <int N, class T>
+Vector<N, T> Vector<N, T>::operator -(const Vector<N, T>& v) const
+{
+    return Vector<N, T>(*this) -= v;
+}
+
+template <int N, class T>
+Vector<N, T> Vector<N, T>::operator *(const T& c) const
+{
+    return Vector<N, T>(*this) *= c;
+}
+
+template <int N, class T>
+Vector<N, T> Vector<N, T>::operator /(const T& c) const
+{
+    return Vector<N, T>(*this) /= c;
+}
+
+template <int N, class T>
+inline bool Vector<N, T>::operator ==(const Vector<N, T>& v) const
+{
+    return std::memcmp(this->_element, v._element, N * sizeof(T)) == 0;
+}
+
+template <int N, class T>
+inline bool Vector<N, T>::operator !=(const Vector<N, T>& v) const
+{
+    return std::memcmp(this->_element, v._element, N * sizeof(T)) != 0;
+}
+
+template <int N, class T>
+Vector<N, T> Vector<N, T>::operator +=(const Vector<N, T>& v) 
+{
+    for(int i = 0; i < N; ++i)
+    {
+        (*this)[i] += v[i];
+    }
+    return (*this);
+}
+
+template <int N, class T>
+Vector<N, T> Vector<N, T>::operator -=(const Vector<N, T>& v)
+{
+    for(int i = 0; i < N; ++i)
+    {
+        (*this)[i] -= v[i];
+    }
+    return (*this);
+}
+
+template <int N, class T>
+Vector<N, T> Vector<N, T>::operator *=(const T& c)
+{
+    for(int i = 0; i < N; ++i)
+    {
+        (*this)[i] *= c;
+    }
+    return (*this);
+}
+
+template <int N, class T>
+Vector<N, T> Vector<N, T>::operator /=(const T& c)
+{
+    for(int i = 0; i < N; ++i)
+    {
+        (*this)[i] /= c;
+    }
+    return (*this);
+}
 
 typedef Vector<2, unsigned> Vector2u;
 typedef Vector<3, unsigned> Vector3u;
@@ -263,4 +279,4 @@ typedef Vector<2, double>   Vector2lf;
 typedef Vector<3, double>   Vector3lf;
 typedef Vector<4, double>   Vector4lf;
 
-#endif /* VECTOR_HH */
+#endif
